eed0e0bc8ca89ca419d90f31378bec03a0865899
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-06 00:15:16 -0500

    Search function implemented

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import './App.css';
 
 class App extends Component {
 
+  // Classes always run the constructor class first. The only thing you'll do with the constructor 
+  // is initialize the state.
+
   constructor() {
     super();
 
     this.state = {
       monsters: []
       
-    }
+    };
+    console.log('constructor');
   }
 
+  // Runs right after the UI component mounts with the render method. This happens third.
   componentDidMount() {
+    console.log('component did mount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => 
@@ -29,9 +35,27 @@ class App extends Component {
     );
   }
 
+  // The render runs second and it determines what to show on the browser. Renders/ mounts the initial UI.
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type="search" 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(event.target.value);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters}
+            })
+          }} 
+        />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}>

f464fb7002d9a86013a6f9ade0d9ac388232451e
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-07 00:09:12 -0500

    Destructured variables out to remove the "this" keyword from multiple locations

--- src/App.js
+++ src/App.js
@@ -35,11 +35,22 @@ class App extends Component {
     );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase(); 
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   // The render runs second and it determines what to show on the browser. Renders/ mounts the initial UI.
   render() {
     console.log('render');
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -48,13 +59,7 @@ class App extends Component {
           className='search-box' 
           type="search" 
           placeholder='search monsters' 
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            
-            this.setState(() => {
-              return { searchField }
-            })
-          }} 
+          onChange={onSearchChange}
         />
         {
           filteredMonsters.map((monster) => {

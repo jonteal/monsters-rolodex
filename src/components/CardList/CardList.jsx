9c3777789a656087a85596d14c8f7fc86aab75e7
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-07 21:58:28 -0500

    Card component created and passing props

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Card from "../Card/Card";
 import './cardList.css';
 
 class CardList extends Component {
@@ -10,14 +11,8 @@ class CardList extends Component {
         return (
             <div className="card-list">
                 {monsters.map((monster) => {
-                    const { name, email, id } = monster;
-
                     return (
-                    <div key={id} className="card-container">
-                        <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${monster.name}`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
+                        <Card monster={monster} />
                 )})}
             </div>
         )
